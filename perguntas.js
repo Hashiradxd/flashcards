951d9db3eb6e22db53e4fffd1b2a299f6ed5323f
hashira_00 <natanaelbt1xdossantos@gmail.com>
2024-11-27 10:50:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Astronomia',
+    'O que é Astronomia?',
+    'Astronomia é a ciência que estuda os corpos celestes e os fenômenos do universo.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Japão?',
+    'A capital do Japão é Tokio'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'Bom dia em ingles é Good morning'
+)
+
+criaCartao(
+    
+)
+
+
+
+
+
+
+
+
